06784a3070ee621bd92bf2ddb6ec9f3262b89aea
John Silver <johncarlosilver@gmail.com>
2020-09-10 17:36:52 -0300

    loading bonito

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import 'semantic-ui-css/semantic.min.css'
+import { Loader } from 'semantic-ui-react'
 import fetch from 'node-fetch'
 import {
   ContactCard
@@ -9,7 +11,25 @@ function randomInt (min, max) {
   return min + Math.floor((max - min) * Math.random())
 }
 
-function App () {
+const listContacts = (contacts) => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap' }}>
+      {
+        contacts.map((contact, index) => (
+          <ContactCard
+            key={`Contact-${index}`}
+            avatar={contact.picture.large}
+            name={contact.name.first + ' ' + contact.name.last}
+            email={contact.email}
+            age={contact.dob.age}
+          />
+        ))
+      }
+    </div>
+  )
+}
+
+export default () => {
   const [contacts, setContacts] = useState([])
   const [countResults, setCountResults] = useState(3)
   const [loading, setLoading] = useState(false)
@@ -20,31 +40,23 @@ function App () {
       .then(response => response.json())
       .then(data => {
         console.log(data)
-        setLoading(false)
         setContacts(data.results)
+      })
+      .finally(_ => {
+        setLoading(false)
       })
   }, [countResults])
 
   return (
     <>
       {
-        loading ? <> <span style={{ padding: '10px' }}> loading </span> <br /> </>
-          : contacts.map((contact, index) => (
-            <ContactCard
-              key={`Contact-${index}`}
-              avatar={contact.picture.large}
-              name={contact.name.first + ' ' + contact.name.last}
-              email={contact.email}
-              age={contact.dob.age}
-            />
-          ))
+        loading ? <Loader active size='massive' />
+          : listContacts(contacts)
       }
 
-      <button onClick={() => setCountResults(randomInt(1, 5))}>
+      <button onClick={() => setCountResults(randomInt(10, 30))}>
           Clique aqui para randomizar uma quantidade de resultados aleatória
       </button>
     </>
   )
 }
-
-export default App
